df44d2e7e4cc60642940d940e062619beed3aaee
Ohad Esperansa <ohad999@gmail.com>
2025-03-25 22:54:27 +0200

    Add project files.

---
+++ SchoolJanitor/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SchoolJanitor
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<double> _bags;
+        private ObservableCollection<string> _trips;
+        private string _bagWeight;
+        private string _tripCount;
+
+        public ObservableCollection<double> Bags { get => _bags; set { _bags = value; OnPropertyChanged(nameof(Bags)); } }
+        public ObservableCollection<string> Trips { get => _trips; set { _trips = value; OnPropertyChanged(nameof(Trips)); } }
+        public string BagWeight { get => _bagWeight; set { _bagWeight = value; OnPropertyChanged(nameof(BagWeight)); } }
+        public string TripCount { get => _tripCount; set { _tripCount = value; OnPropertyChanged(nameof(TripCount)); } }
+
+        public ICommand AddBagCommand { get; }
+        public ICommand CalculateTripsCommand { get; }
+
+        public MainViewModel()
+        {
+            Bags = new ObservableCollection<double>();
+            Trips = new ObservableCollection<string>();
+            AddBagCommand = new RelayCommand(AddBag);
+            CalculateTripsCommand = new RelayCommand(CalculateTrips);
+        }
+
+        private void AddBag()
+        {
+            if (double.TryParse(BagWeight, out double weight) && weight >= 1.01 && weight <= 3.0)
+            {
+                Bags.Add(weight);
+                BagWeight = string.Empty;
+            }
+        }
+
+        private void CalculateTrips()
+        {
+            var result = CalculateMinTrips(Bags.ToList());
+            TripCount = $"Minimum number of trips: {result.Item1}";
+            Trips.Clear();
+            int tripNumber = 1;
+            foreach (var trip in result.Item2)
+            {
+                Trips.Add($"Trip {tripNumber++}: {string.Join(", ", trip)} kg");
+            }
+        }
+
+        private (int, List<List<double>>) CalculateMinTrips(List<double> bagWeights)
+        {
+            List<List<double>> trips = new List<List<double>>();
+            bagWeights = bagWeights.OrderByDescending(x => x).ToList();
+            while (bagWeights.Count > 0)
+            {
+                double currentWeight = 0;
+                List<double> trip = new List<double>();
+                var firstBag = 0;
+                while (firstBag < bagWeights.Count && currentWeight + bagWeights[firstBag] <= 3.0)
+                {
+                    currentWeight += bagWeights[firstBag];
+                    trip.Add(bagWeights[firstBag]);
+                    bagWeights.RemoveAt(firstBag);
+                }
+                trips.Add(trip);
+            }
+            return (trips.Count, trips);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        public RelayCommand(Action execute) => _execute = execute;
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => _execute();
+    }
+}
---
+++ SchoolJanitor/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SchoolJanitor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
